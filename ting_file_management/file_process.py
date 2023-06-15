9bd5d7d57dc46dbe2a086cda170499d374561d46
nickolasvm <nickolas64vm@gmail.com>
2023-06-15 17:18:51 -0300

    feat: Implemented function process on file_process module

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,26 @@
+from ting_file_management.file_management import txt_importer
+
+# from queue import Queue
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    content_list = txt_importer(path_file)
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(content_list),
+        "linhas_do_arquivo": content_list,
+    }
+
+    if len(instance) > 0:
+        for i in range(len(instance)):
+            if (
+                instance.search(i)["nome_do_arquivo"]
+                == result["nome_do_arquivo"]
+            ):
+                return
+
+    instance.enqueue(result)
+    print(result)
 
 
 def remove(instance):
@@ -8,3 +29,9 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
+
+# path = "statics/nome_pedro.txt"
+# queue = Queue()
+
+# process(path, queue)
