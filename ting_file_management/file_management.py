b0365d55e3e7e84a8d58c74e288dfd6b97ed7716
nickolasvm <nickolas64vm@gmail.com>
2023-06-15 14:29:52 -0300

    feat: Implemented module file_management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    _, extension = path_file.split(".")
+    if extension != "txt":
+        sys.stderr.write("Formato inválido")
+        return
+
+    try:
+        with open(path_file, "r") as file:
+            content = file.read().split("\n")
+            return content
+
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
