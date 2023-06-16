baaf2a59bf07a5473a58b6404a94641a9183007d
nickolasvm <nickolas64vm@gmail.com>
2023-06-16 18:45:40 -0300

    refactor: Refactored the test function for PriorityQueue class to show what exactly is being tested

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -7,17 +7,20 @@ from ting_file_management.file_process import process
 def test_basic_priority_queueing():
     queue = PriorityQueue()
 
+    # test if file with 5 lines or more queue in regular priority
     process("statics/arquivo_teste_maior.txt", queue)
     assert len(queue.regular_priority) == 1
+    assert not queue.is_priority(queue.search(0))
 
+    # test if file with less than 5 lines queue in high priority
     process("statics/arquivo_teste.txt", queue)
     assert len(queue.high_priority) == 1
+    assert queue.is_priority(queue.search(0))
 
+    # test if dequeue remove the priority file first
     queue.dequeue()
     assert len(queue.high_priority) == 0
 
+    # test if search with a index that does not exist, it throws an error
     with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
         queue.search(3)
-
-    process("statics/arquivo_teste.txt", queue)
-    assert queue.is_priority(queue.search(0))
