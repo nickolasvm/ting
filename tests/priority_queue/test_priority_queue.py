1c82dd9d798b379af9d19459595d2ed61a41d25a
nickolasvm <nickolas64vm@gmail.com>
2023-06-16 18:10:25 -0300

    feat: implemented tests for PriorityQueue class

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,23 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+
+from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.file_process import process
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queue = PriorityQueue()
+
+    process("statics/arquivo_teste_maior.txt", queue)
+    assert len(queue.regular_priority) == 1
+
+    process("statics/arquivo_teste.txt", queue)
+    assert len(queue.high_priority) == 1
+
+    queue.dequeue()
+    assert len(queue.high_priority) == 0
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        queue.search(3)
+
+    process("statics/arquivo_teste.txt", queue)
+    assert queue.is_priority(queue.search(0))
